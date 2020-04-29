5d94600f9239fc13054b6fb1a0de3408dc0f7f50
Kafin Salim <kafinsalim@gmail.com>
2020-04-29 23:12:59 +0700

    setting up packages & directory

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @flow
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
-function App() {
+const client = new ApolloClient({
+  uri: "https://graphql-pokemon.now.sh",
+});
+
+function App(): React.Node {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div>App</div>
+    </ApolloProvider>
   );
 }
 
