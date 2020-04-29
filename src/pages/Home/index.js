3ba80e250b3d6b545d5edda622f0448c80006d21
Kafin Salim <kafinsalim@gmail.com>
2020-04-30 02:27:18 +0700

    create infinite list, auto fetch another pokemon on home screen when scroll reach bottom

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,24 +7,48 @@ import PokemonCards from "./PokemonCards";
 import Error from "../../components/Error";
 
 export default function Home(): React.Node {
-  const { loading, error, data } = useQuery(PokemonList, {
-    variables: {
-      first: 15,
-    },
+  const [variables, setVariables] = React.useState({
+    first: 18,
+  });
+  const { loading, error, data, fetchMore } = useQuery(PokemonList, {
+    variables,
   });
 
-  if (error) return <Error />;
+  const handleScroll = React.useCallback(() => {
+    const scrollTop = document.documentElement.scrollTop;
+    const offsetHeight = document.documentElement.offsetHeight;
+    if (window.innerHeight + scrollTop < offsetHeight - 100) return;
+
+    fetchMore({
+      variables: {
+        ...variables,
+        first: variables.first + 12,
+      },
+      updateQuery: (prev, { fetchMoreResult, variables }) => {
+        setVariables(variables);
+        if (!fetchMoreResult) return prev;
+        return Object.assign({}, prev, {
+          pokemons: [...fetchMoreResult.pokemons],
+        });
+      },
+    });
+  }, [fetchMore, variables]);
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
 
   const pokemonsFetched = data ? data.pokemons : [];
+  const limitExceed = pokemonsFetched.length > 150; // our graphql api has limit
+
+  if (error) return <Error />;
 
   return (
     <div>
       <h2>Pokédex Pintar</h2>
-      {loading ? (
-        <SpinningPokeball />
-      ) : (
-        <PokemonCards pokemons={pokemonsFetched} />
-      )}
+      <PokemonCards pokemons={pokemonsFetched} />
+      {!limitExceed && <SpinningPokeball />}
     </div>
   );
 }
